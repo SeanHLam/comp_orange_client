94005b3e4ab568c4f85bfdf07cd8589c34dff9b7
CanSanchez <asanchez24@my.bcit.ca>
2022-11-21 15:12:26 -0800

    add followers/following box and follower avatar

---
+++ orangeapp/pages/comps/followbox.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { FlexBox } from '../../styles/globals'
+import { Button } from '@mui/material'
+import FollowAvatarComp from './followavatar'
+
+export default function FollowBoxComp({
+    title = 'Followers'
+}) {
+  return (
+    <FlexBox dir="column">
+        <h2
+            style={{
+                fontSize: '1em',
+                alignSelf: 'flex-start',
+                margin: '2%'
+            }}>
+            {title}
+        </h2>
+        <FlexBox style={{
+            minWidth: '100%',
+            justifyContent: 'flex-start'
+        }}>
+                <FollowAvatarComp></FollowAvatarComp>
+                <FollowAvatarComp></FollowAvatarComp>
+        </FlexBox>
+  </FlexBox>
+  )
+}
--- orangeapp/pages/comps/spritzboard.js
+++ orangeapp/pages/comps/spritzboard.js
@@ -17,6 +17,8 @@ export default function PostDashboardComp() {
         <TweetComp></TweetComp>
         <TweetComp></TweetComp>
         <TweetComp></TweetComp>
+        <TweetComp></TweetComp>
+        <TweetComp></TweetComp>
   </FlexBox>
   )
 }
