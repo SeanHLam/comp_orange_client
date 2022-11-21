b8dce2decde1f6e381d100cea40fb858b0209980
CanSanchez <asanchez24@my.bcit.ca>
2022-11-21 13:08:13 -0800

    add header component

--- orangeapp/pages/dashboard.js
+++ orangeapp/pages/dashboard.js
@@ -4,6 +4,8 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import { Button } from "@mui/material";
+import HeaderComp from "./comps/header";
+
  
 // Basic dashboard structure - to be updated
 export default function Home() {
@@ -15,8 +17,9 @@ export default function Home() {
        <Grid item xs={12}>
          <FlexBox>
 {/* Header containing logo, profile and some buttons*/}
-           <FlexBox>Logo</FlexBox>
-           <FlexBox>Buttons</FlexBox>
+            <HeaderComp></HeaderComp>
+           {/* <FlexBox>Logo</FlexBox>
+           <FlexBox>Buttons</FlexBox> */}
          </FlexBox>
        </Grid>
        <Grid item xs={6}>
